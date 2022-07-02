14ab83d954cf916cf4dd45cf3260c564f9be9f8a
Antuanett-Lephebvre <72161650+Antuanett-Lephebvre@users.noreply.github.com>
2022-07-03 01:46:29 +0300

    Update and rename frontend/src/utils/constants.js to интерфейс/Src/утилиты/constants.js

--- интерфейс/Src/утилиты/constants.js
+++ интерфейс/Src/утилиты/constants.js
@@ -1,5 +1,5 @@
 //const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'http://api.kate.diplom.nomoreparties.sbs';
+const BASE_URL = 'https://api.kate.diplom.nomoreparties.sbs';
 const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 const SHORTMOVIES_DURATION = 40;
 const DEVICE_PARAMS = {
